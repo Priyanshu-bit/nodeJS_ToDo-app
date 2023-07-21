b361c7d78c9068f8aceb09d06e948a293ec4da97
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-07-21 18:22:16 +0530

    cors update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import userRouter from "./Routes/user.js";
 import taskRouter from "./Routes/task.js";
 import {config} from  "dotenv"
 import cookieParser from "cookie-parser";
-import Jwt from "jsonwebtoken";
+
 import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors"
 
@@ -15,16 +15,31 @@ config({
 
 
 
+
+// app.use(cors({
+//   origin:[process.env.FRONTEND_URL],
+//   methods :["GET","PUT","POST","DELETE"],
+//   credentials:true
+// }));
+const allowedOrigins = ['http://localhost:5173'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Enable sending cookies in the request (if needed)
+}));
+
 app.use(express.json());
 
 app.use (cookieParser());
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
-app.use(cors({
-  origin:[process.env.FRONTEND_URL],
-  methods :["GET","PUT","POST","DELETE"],
-  credentials:true
-}));
 
 app.get("/", (req, res) => {
   res.send("Nice working");
