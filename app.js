592ed5b84766124f49a913ba2b49827209d4523a
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-07-24 13:20:41 +0530

    add

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const frontendURL = 'http://localhost:5173';
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (origin === frontendURL) {
+    if (origin === "*") {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
